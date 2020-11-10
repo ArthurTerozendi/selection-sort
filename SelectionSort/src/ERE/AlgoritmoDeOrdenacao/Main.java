3a25b01483558b6279b34287889649a2d9c13999
DrokeRavens <droke.ravens@gmail.com>
2020-11-10 13:04:06 -0300

    Organização do Codigo - Criação de Metodos para impressão e selection sort

--- SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Main.java
+++ SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Main.java
@@ -5,59 +5,82 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
-
         Leitura leitura = new Leitura();
         float cotacao[] = leitura.lerCotacao();
+        PrintArray(cotacao);
+        SelectionSort(cotacao);
+        PrintArray(cotacao);
 
-        for (int i = 0; i < cotacao.length; i++) {
-            System.out.print(cotacao[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < cotacao.length - 1; i++) {
-            int menorIndex = i;
-            for (int j = i; j < cotacao.length; j++) {
-                if (cotacao[menorIndex] > cotacao[j]) {
-                    menorIndex = j;
-                }
-            }
-            if (cotacao[i] > cotacao[menorIndex]){
-                float aux = cotacao[i];
-                cotacao[i] = cotacao[menorIndex];
-                cotacao[menorIndex] = aux;
-            }
-        }
-
-        for (int i = 0; i < cotacao.length; i++) {
-            System.out.print(cotacao[i] + " ");
-        }
-
-        System.out.println();
 
         String nomes[] = leitura.lerNomes();
+        PrintArray(nomes);
+        SelectionSort(nomes);
+        PrintArray(nomes);
+    }
 
-        for (int i = 0; i < nomes.length; i++) {
-            System.out.println(nomes[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < nomes.length - 1; i++) {
+    /**
+     * Utilizando o algoritmo SelectionSort, a cadeia é ordenada de forma alfabetica
+     * @param arr A array a ser ordenada
+     */
+    public static void SelectionSort(String arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int menorIndex = i;
-            for (int j = i; j < nomes.length; j++) {
-                if (nomes[menorIndex].compareTo(nomes[j]) > 0) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[menorIndex].compareTo(arr[j]) > 0) {
                     menorIndex = j;
                 }
             }
-            if (nomes[i].compareTo(nomes[menorIndex]) > 0){
-                String aux = nomes[i];
-                nomes[i] = nomes[menorIndex];
-                nomes[menorIndex] = aux;
+            if (arr[i].compareTo(arr[menorIndex]) > 0) {
+                String _aux = arr[i];
+                arr[i] = arr[menorIndex];
+                arr[menorIndex] = _aux;
             }
         }
+    }
 
-        for (int i = 0; i < nomes.length; i++) {
-            System.out.println(nomes[i] + " ");
+    /**
+     * Utilizando o algoritmo SelectionSort, a cadeia é ordenada de crescente
+     * @param arr A array a ser ordenada
+     */
+    public static void SelectionSort(float[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int menorIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[menorIndex] > arr[j]) {
+                    menorIndex = j;
+                }
+            }
+            if (arr[i] > arr[menorIndex]){
+                float _aux = arr[i];
+                arr[i] = arr[menorIndex];
+                arr[menorIndex] = _aux;
+            }
         }
+    }
 
+    /**
+     * Imprime uma array de Strings para o usuario.
+     * @param arr A array a ser imprimida
+     */
+    public static void PrintArray(String[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * Imprime uma array de float para o usuario.
+     * @param arr A array a ser imprimida
+     */
+    public static void PrintArray(float[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
         System.out.println();
     }
 }
